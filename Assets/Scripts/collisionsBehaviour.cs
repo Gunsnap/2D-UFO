e936955f1929b576f0f333c323029f71d5c9c563
HvadSkerDer <ThomasKrogh-@hotmail.com>
2016-05-23 13:10:24 +0200

    Det er der

---
+++ Assets/Scripts/collisionsBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionsBehaviour : MonoBehaviour {
+	
+	void Start() {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+			Debug.Log ("Collision!");
+		}
+		if (collision.relativeVelocity.magnitude > 2)
+			Debug.Log ("Eksemplet ville afspille lyd nu");
+	}
+}
