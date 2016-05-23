08a51395bdc1311298eead846991d912df54abc7
HvadSkerDer <ThomasKrogh-@hotmail.com>
2016-05-23 10:34:21 +0200

    expl

--- Assets/Scripts/explodeScr.cs
+++ Assets/Scripts/explodeScr.cs
@@ -4,15 +4,33 @@ using System.Collections;
 public class explodeScr : MonoBehaviour {
 	public float explodeSec = 2.5f;
 	public float putTime;
+	public float bombDelay = 3.5f;
+	public float timeNow;
 
+	private Spawner spawnFire;
+
+	Vector3 BombPosition;
 
 	void Start() {
-		removeBomb ();
 		putTime = Time.time;
+		var BombObject = GameObject.Find (this.gameObject);
+		BombPosition = BombObject.transform.position;
+	}
+
+	void Update() {
+		timeNow = Time.time;
+		if(timeNow > putTime+bombDelay) {
+			removeBomb ();
+		}
 	}
 
 	public void removeBomb() {
 		Debug.Log ("Fjern bombe");
+		DestroyObject (this.gameObject);
+	}
+
+	public void blastHallWithFire() {
+		spawnFire.SpawnSomethingAwesome (BombPosition);
 	}
 
 }
