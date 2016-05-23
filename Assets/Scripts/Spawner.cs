c13c3147546af8be61a8ab2739bd9946c1328c4c
HvadSkerDer <ThomasKrogh-@hotmail.com>
2016-05-23 09:56:49 +0200

    SpawnScript - "done"

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,21 +6,11 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
-	//public Transform[] spawnLocations;
 	public GameObject[] whatToSpawnPrefab;
 	public GameObject[] whatToSpawnClone;
 
-	/*
-	void Start() {
-		SpawnSomethingAwesome ();
+	public void SpawnSomethingAwesome(Vector3 SpawnPos){
+		Debug.Log(SpawnPos);
+		whatToSpawnClone [0] = Instantiate (whatToSpawnPrefab[0],SpawnPos, Quaternion.Euler(0,0,0)) as GameObject;
 	}
-	*/
-
-	public void SpawnSomethingAwesome(Vector3 PlayerPos){
-	//	whatToSpawnClone [0] = Instantiate (whatToSpawnPrefab[0],spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
-	//	whatToSpawnClone [1] = Instantiate (whatToSpawnPrefab[1],spawnLocations[1].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
-		Debug.Log(PlayerPos);
-		whatToSpawnClone [2] = Instantiate (whatToSpawnPrefab[2],PlayerPos, Quaternion.Euler(0,0,0)) as GameObject;
-	}
-
 }
