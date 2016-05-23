8dffda16fe3dae7da780189d977f9e13d41fd7ee
Gunsnap <gunsnap@gmail.com>
2016-05-23 10:30:12 +0200

    Block spawner ting når forsvinder

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,8 +9,8 @@ public class Spawner : MonoBehaviour {
 	public GameObject[] whatToSpawnPrefab;
 	public GameObject[] whatToSpawnClone;
 
-	public void SpawnSomethingAwesome(Vector3 SpawnPos){
-		Debug.Log(SpawnPos);
-		whatToSpawnClone [0] = Instantiate (whatToSpawnPrefab[0],SpawnPos, Quaternion.Euler(0,0,0)) as GameObject;
+	public void SpawnSomethingAwesome (Vector3 SpawnPos, int element = 0) {
+		Debug.Log (SpawnPos);
+		whatToSpawnClone [element] = Instantiate (whatToSpawnPrefab [element], SpawnPos, Quaternion.Euler (0, 0, 0)) as GameObject;
 	}
 }
